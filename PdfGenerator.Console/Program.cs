84c9d1c714f3575bd9c7e948d818340d4004e44c
gabrielmedsilva <gabrielmedsilva@outlook.com>
2025-03-26 15:37:46 -0300

    feat: Testes de geração do PDF

--- PdfGenerator.Console/Program.cs
+++ PdfGenerator.Console/Program.cs
@@ -1,20 +1,22 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using QuestPDF.Companion;
-using QuestPDF.Fluent;
+﻿using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
-using QuestPDF.Previewer;
 
-QuestPDF.Settings.License = LicenseType.Community;
-
-Document.Create(container => 
+class Program
 {
-    container.Page(page => 
+    static void Main()
     {
-        page.Size(PageSizes.A4);
-         page.Margin(value:2, Unit.Centimetre);
-         page.PageColor(Colors.White);
-         page.DefaultTextStyle(handler:x => x.FontSize(20));
-    });
-})//Document
-.ShowInCompanion();
+        // Definir a licença para evitar erros
+        QuestPDF.Settings.License = LicenseType.Community;
+
+        Document.Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Content().Text("Olá, este é um PDF gerado com QuestPDF!");
+            });
+        }).GeneratePdf("output.pdf");
+
+        Console.WriteLine("PDF gerado com sucesso!");
+    }
+}
