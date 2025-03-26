f28605f09597c8c4770d540eb235204d71d2cc99
gabrielmedsilva <gabrielmedsilva@outlook.com>
2025-03-25 23:03:10 -0300

    feat: Teste v1 da visualização

--- PdfGenerator.Console/Program.cs
+++ PdfGenerator.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+using QuestPDF.Companion;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+
+QuestPDF.Settings.License = LicenseType.Community;
+
+Document.Create(container => 
+{
+    container.Page(page => 
+    {
+        page.Size(PageSizes.A4);
+         page.Margin(value:2, Unit.Centimetre);
+         page.PageColor(Colors.White);
+         page.DefaultTextStyle(handler:x => x.FontSize(20));
+    });
+})//Document
+.ShowInCompanion();
